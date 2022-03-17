9d5ef9f8c88066f0abef664c68b82ea16f034604
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-17 11:15:28 +0530

    Update LineComparision.java

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -11,9 +11,9 @@ static float length1=0;
         static void compare() {
 
                 if ( length1> length2) {
-                        System.out.println("Line 2 is greater than Line 1");
+                        System.out.println("Line 1 is smaller than Line 2");
                 } else {
-                        System.out.println("Line 2 is smaller than Line 1");
+                        System.out.println("Line 1 is greater than Line 2");
                 }
         }
 

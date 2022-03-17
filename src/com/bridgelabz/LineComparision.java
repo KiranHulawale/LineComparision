5a4b6901103f5199e2e584ff0cd7c6c8ca1db94a
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-17 11:07:52 +0530

    Update LineComparision.java

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -5,20 +5,42 @@ import java.lang.Math;
 
 public class LineComparision {
 
-    public static void main(String[] args) {
-
-        // welcome to line comparison program.
-        double lengthLine = 0;
+   static void equals(){
+        // welcome to Line Comparison Program.
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the numbers");
+        System.out.println("Enter x1");
         double x1 = sc.nextDouble();
-        double x2 = sc.nextDouble();
-
+        System.out.println("Enter y1");
         double y1 = sc.nextDouble();
+        System.out.println("Enter x2");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter y2");
         double y2 = sc.nextDouble();
+        System.out.println("Enter x3");
+        double x3 = sc.nextDouble();
+        System.out.println("Enter y3");
+        double y3 = sc.nextDouble();
+        System.out.println("Enter x4");
+        double x4 = sc.nextDouble();
+        System.out.println("Enter y4");
+        double y4 = sc.nextDouble();
 
-        // Calculating lenth between co-ordinates.
-         lengthLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of line is: " + lengthLine);
+
+        double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of line is: " + length1);
+
+        double length2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Length of line is: " + length2);
+
+        if (length1 == length2) {
+            System.out.println("Two lines are equal");
+        } else {
+            System.out.println("Two lines are not equal");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        equals();
     }
 }

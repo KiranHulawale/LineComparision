40e190df7722d0ef7c54c393da13aabfe809cb79
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-17 10:46:09 +0530

    Update LineComparision.java

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -1,5 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class LineComparision {
     
+    public static void main(String[] args) {
+
+        // welcome to Line Comparison Program.
+        double lengthLine = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        double x1 = sc.nextDouble();
+        double x2 = sc.nextDouble();
+
+        double y1 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+
+        // Calculating length between co-ordinates.
+         lengthLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of line is: " + lengthLine);
+    }
 }
